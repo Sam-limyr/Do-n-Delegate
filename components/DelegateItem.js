d69da15e0ac20f44b12eb79363e58cd9ff75fdff
unknown <nacuniel@gmail.com>
2019-07-11 17:33:13 +0800

    Dialog items created and styled.

---
+++ components/DelegateItem.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import Dialog from "react-native-dialog";
+ 
+export default class DelegateItem extends Component {
+  state = {
+    dialogVisible: false,
+    name : "Placeholder Name"
+  };
+ 
+  showDialog = () => {
+    this.setState({ dialogVisible: true });
+  };
+ 
+  handleCancel = () => {
+    this.setState({ dialogVisible: false });
+  };
+ 
+  handleSubmit = () => {
+    // perform backend stuff
+    this.setState({ dialogVisible: false });
+  };
+
+  setName = (inputName) => {
+    this.setState({ name: inputName.toString()})
+  }
+ 
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={this.showDialog} style={styles.button}>
+            <Text style={styles.words}>{ this.state.name.toString() }</Text>
+        </TouchableOpacity>
+        <Dialog.Container visible={this.state.dialogVisible}>
+          <Dialog.Title children="Delegate a Task and Deadline" />
+          <Dialog.Input wrapperStyle={styles.inputBox} />
+          <Dialog.Input wrapperStyle={styles.inputBox} />
+          <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+          <Dialog.Button label="Submit" onPress={this.handleSubmit} />
+        </Dialog.Container>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+container: {
+    justifyContent: 'center',
+    flex: 1,
+    paddingHorizontal: 40,
+    paddingVertical: 50
+},
+button: {
+    alignItems: 'center',
+    backgroundColor: '#FFBB00',
+    borderBottomEndRadius: 10,
+    borderBottomStartRadius: 10,
+    borderColor: '#FFAA00',
+    borderTopEndRadius: 10,
+    borderTopStartRadius: 10,
+    borderWidth: 4,
+    marginHorizontal: 40,
+    marginVertical: 10,
+    padding: 25
+},
+inputBox: {
+    height: 40,
+    borderColor: '#DDDDDD',
+    borderWidth: 1
+},
+words: {
+    color: '#333333',
+    fontSize: 16,
+    fontWeight: 'bold',
+}
+})
